a43016c918c6c5690bf07cf7bc0003eccb01b27f
Karmveer Singh <karmveer.nz@gmail.com>
2024-06-27 20:08:28 +1200

    Social media and nav links done

--- frontend/src/components/Nav/index.jsx
+++ frontend/src/components/Nav/index.jsx
@@ -26,6 +26,50 @@ const Nav = () => {
           <img src={LogoS} alt="logo" />
         </Link>
 
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+          </NavLink>
+
+          <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+          </NavLink>
+
+          <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+          </NavLink>
+        </nav>
+
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/karmveer-singh-474056236/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://github.com/Karmveer27"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+      </ul>
+
         
     </div>
   );
