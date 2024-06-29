8c8668d255e41abd664164d0cfbfbbd10bf9230b
Karmveer Singh <karmveer.nz@gmail.com>
2024-06-29 20:30:37 +1200

    Fixed about us page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from "./components/Home/index.jsx"
+import About from "./components/About/index.jsx"
 
 import Layout from './components/Layout'
 
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
       
         </Route>
       </Routes>
